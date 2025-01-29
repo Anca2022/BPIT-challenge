db7366aa7b31a51f2554bfdc312fa39ce0810e01
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2025-01-29 23:29:01 +0200

    done TransactionsList

--- src/components/transactionsList/TransactionsList.tsx
+++ src/components/transactionsList/TransactionsList.tsx
@@ -1,9 +1,15 @@
 import TransactionsListItem from "./TransactionsListItem";
 import Transaction from "../../types/Transaction";
+import './transactionList.scss';
+
 export default function TransactionsList({transactions} : {transactions: Transaction[]}){
     return(
         <div className='transactions-container'>
             <div className='transaction-list-header'>
+                <p className="col1">Description</p>
+                <p className="col2">Category</p>
+                <p className="col3">Transaction Date</p>
+                <p className="col4">Amount</p>
             </div>
             <ul className='transactions-list'>
                 {
