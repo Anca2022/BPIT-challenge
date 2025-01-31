60f16a76f8a98fbd2bbda77e4da3e18f1129b8ff
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2025-02-01 01:15:32 +0200

    modal done

---
+++ src/components/ctaBtn/CtaBtn.tsx
@@ -0,0 +1,8 @@
+import "./ctaBtn.scss";
+
+interface BtnProps{
+    handleClick?:()=>void;
+}
+export default function CtaBtn({handleClick, children} : React.PropsWithChildren<BtnProps>){
+return <button className='cta-btn' onClick={handleClick}>{children}</button> 
+}
